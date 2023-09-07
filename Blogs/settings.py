3d655173d76fee3ef47af1897eb6e8126bc49129
shakti singh <60788912+shakti1590@users.noreply.github.com>
2023-09-07 09:45:45 +0530

    Update settings.py

--- Blogs/settings.py
+++ Blogs/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7)u9_79tx#-nyv26q$nv&0xoadecq+lvcyd6(zq$_+1kihs)yc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
